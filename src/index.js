4bdc6cbf66d635d8414316a71683db71290fcc2b
infoiczb-hash <info.iczb@gmail.com>
2026-01-30 16:16:37 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async'; // 1. Импорт
+import { HelmetProvider } from 'react-helmet-async'; // <--- ВАЖНО
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider> {/* 2. Обертка */}
+    <HelmetProvider> {/* <--- ОБЯЗАТЕЛЬНО ОБЕРНУТЬ */}
       <BrowserRouter>
         <App />
       </BrowserRouter>
