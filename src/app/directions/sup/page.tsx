274f62636125e0b9b52288107fbb92e3ce9180f3
infoiczb-hash <info.iczb@gmail.com>
2026-02-22 13:56:17 +0200

    Update page.tsx

--- src/app/directions/sup/page.tsx
+++ src/app/directions/sup/page.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Metadata } from 'next';
 import { getTours } from '@/features/tours/api';
-import SupLandingClient from '@/features/directions/sup/SupLanding'
+import SupLandingClient from '@/features/directions/sup/SupLanding';
 
 export const metadata: Metadata = {
   title: 'SUP Серфинг и Прогулки | Турклуб ЭВА',
   description: 'Сапбординг в Молдове и Приднестровье. Обучение, сплавы по Днестру, закаты и SUP-йога. Безопасно и доступно для новичков.',
   openGraph: {
     title: 'Открой мир SUP-серфинга',
-     }
+  }
 };
 
+// Вот этой части не хватало! 
+export default function SupPage() {
+  return (
+    <main className="bg-slate-950 min-h-screen">
+      <SupLandingClient />
+    </main>
+  );
+}
