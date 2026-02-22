6288977cdf3b8c666bcddd4a8baed173187aa7e1
infoiczb-hash <info.iczb@gmail.com>
2026-02-22 14:01:11 +0200

    Update PromoBlock.tsx

--- src/components/layout/PromoBlock.tsx
+++ src/components/layout/PromoBlock.tsx
@@ -145,7 +145,6 @@ export default function PromoBlock() {
         </div>
       </div>
       <ContactHubModal isOpen={isContactOpen} onClose={() => setIsContactOpen(false)} initialTab={contactTab} tourContext={tourContext} />
-      <TourQuizModal isOpen={isQuizOpen} onClose={() => setIsQuizOpen(false)} onResultSelect={handleQuizResult} />
-    </section>
+         </section>
   );
 }
